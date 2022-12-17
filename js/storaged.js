082d8d726f895afdb89f78f96f19cab212defe50
dayanjr.github.io <91074122+dayanjr@users.noreply.github.com>
2022-12-17 03:40:55 -0600

    Add files via upload

--- js/storaged.js
+++ js/storaged.js
@@ -1,4 +1,11 @@
 function show(){
-    document.getElementById("result").innerHTML = "You have ordered " + localStorage.clickcount + "drink(s) so far.";
+    console.log("m",localStorage.clickcount);
+    if( localStorage.clickcount == 0)
+    {
+        document.getElementById("result").innerHTML = "You have not ordered any drink(s) so far."; 
+    }else{
+        document.getElementById("result").innerHTML = "You have ordered " + localStorage.clickcount + " drink(s) so far.";
+    }
+    
 }
 show();
